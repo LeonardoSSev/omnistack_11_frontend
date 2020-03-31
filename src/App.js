7d680a28345e9d0ddebd926ca677f3214d7939ec
leonardossev <leonardossev@gmail.com>
2020-03-31 20:59:56 -0300

    feat(setup): define rotas e estiliza aplicação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Routes from './router';
+
+import './global.css';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Routes/>
     </>
   );
 }
