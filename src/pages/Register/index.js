9e572caefc2d5ebf58201a80d085359adb4126ec
leonardossev <leonardossev@gmail.com>
2020-03-31 21:22:03 -0300

    feat(pages): adiciona tela de cadastro

---
+++ src/pages/Register/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import './styles.css';
+
+export default function Register() {
+  return (
+    <h1 className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be the Hero"/>
+
+          <h1>Cadastro</h1>
+          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041"/>
+            Já tenho cadastro
+          </Link>
+        </section>
+
+        <form>
+          <input type="text" placeholder="Nome da ONG"/>
+          <input type="email" placeholder="E-mail"/>
+          <input type="text" placeholder="WhatsApp"/>
+          
+          <div className="input-group">
+            <input type="text" placeholder="Cidade"/>
+            <input type="text" placeholder="UF" style={{ width: 80 }} />
+          </div>
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </h1>
+  )
+};
